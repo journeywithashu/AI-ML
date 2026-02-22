211dac27be705cbd97e540ed1513809e5eca52a7
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2026-02-22 22:54:34 +0530

    Dictionary method

--- Day-04/Dictionary.py
+++ Day-04/Dictionary.py
@@ -4,6 +4,22 @@ info = {
      "subject":["math","science"]
 }
 
+#dictionary method
+dict_keys = list(info.keys())
+print(dict_keys)
+
+dict_vals = list(info.values())
+print(dict_vals)
+
+print(info.get("cgpa2"))
+
+info.update(
+     {
+          "city":"delhi"
+     }
+)
+print(info)
+
 info["cgpa"] = 9.8
 print(info["cgpa"])
 
