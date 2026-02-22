c18664a7639107fbd8cc93cbff5243398e42342f
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2026-02-22 21:26:27 +0530

    String formatting

--- Day-04/string.py
+++ Day-04/string.py
@@ -11,3 +11,24 @@ print(sentence)
 word = "I study from itself"
 
 print(word[5:])
+
+#String formatting
+
+a = 5
+b = 10
+sum = a + b
+print("sum is {}".format(sum))
+
+#normal formating
+print("language is {}".format("python"))
+print("sum of {} & {} is {}".format(a, b, sum))
+
+#index based formatting
+print("sum of {1} & {0} is {2}".format(a, b, sum ))
+
+#value based formatting
+print("value of vars {a} & {b}".format(a = 5, b = 10))
+
+#f- string
+
+print(f"sum of {a} & {b} is {a + b}")
