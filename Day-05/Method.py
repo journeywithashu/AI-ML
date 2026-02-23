9f40f02e0044e0fc95db821d3b294fe7133f7aa7
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2026-02-23 23:40:01 +0530

    Static method

--- Day-05/Method.py
+++ Day-05/Method.py
@@ -12,6 +12,11 @@ class Laptop:
      def get_info(self):#instance method
           print(f"laptop has {self.RAM} RAM & {self.storage} {self.storage_type}")
 
-l1 =Laptop("16gb", "512gb")
+     @staticmethod
+     def calc_discount(price, discount):
+         final_price = price - (discount*price/100)
+         print(f"discounted price = {final_price}")
 
-{Laptop.get_storage_type()}
+l1 = Laptop("16gb", "512gb")
+
+l1.calc_discount(40_000,10)
