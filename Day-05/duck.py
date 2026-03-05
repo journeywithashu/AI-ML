e73d3c2a8595176ce1f99adf12387155b2a193b1
Ashutosh Yadav <144024653+journeywithashu@users.noreply.github.com>
2026-03-05 13:13:06 +0530

    duck polymorphism

---
+++ Day-05/duck.py
@@ -0,0 +1,13 @@
+class Teacher():
+     def get_designation(self):
+          print("designation = Teacher")
+
+class Accountant():
+     def get_designation(self):
+          print("designation = Accountant")
+
+t1 = Teacher()
+t1.get_designation()
+
+acc1 = Accountant()
+acc1.get_designation()
